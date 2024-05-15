d9077caab5b68aecab4ea9246267798c4726dff0
coder7475 <robiulhossain7475@gmail.com>
2024-05-15 07:59:30 +0600

    feat: group by gender

--- group-stage.mongodb.js
+++ group-stage.mongodb.js
@@ -1,9 +1,15 @@
 /**
  * * Learn Group stage of aggregation
  * $group aggregation operators: $count, $max, $min, $avg, $sum, $push
+ * $group stage: _id: required - group by field given in _id
  */
 use("practice")
 
 db.test.aggregate([
-
+  // stage - 1
+  {
+    $group: {
+      _id: "$gender",
+    }
+  }  
 ])
