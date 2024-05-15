1cfa7035b85bbd5ca7d10b31b7b4eef90816d7f0
coder7475 <robiulhossain7475@gmail.com>
2024-05-15 22:08:35 +0600

    feat: learn indexing

--- indexing.mongodb.js
+++ indexing.mongodb.js
@@ -5,10 +5,22 @@
 
 use("practice")
 
-db.orders.find({_id: ObjectID("654de5f6198dfb2206026d0c")})
-//   .explain("executionStats")
 
 // COLLSCAN
 // db.test.find().explain("executionStats")
-db.test.find({ email: "mdangl1@odnoklassniki.ru" })
-    .explain("executionStats")
+// db.test.find({ email: "mdangl1@odnoklassniki.ru" })
+    // .explain("executionStats")
+
+// Indexing
+// create index with email
+// db.massive.createIndex({ email: 1 })
+
+// drop index
+db.massive.dropIndex({ email: 1 })
+
+// IXSCAN
+db.massive.find({
+    email: "rosariohickman@snacktion.com"
+})
+.explain("executionStats")
+
