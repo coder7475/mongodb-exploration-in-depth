7a876751002c77560aceeb3efa1d8049b2a9c42d
coder7475 <robiulhossain7475@gmail.com>
2024-05-17 21:08:43 +0600

    feat: first step to solve task 6

---
+++ practice-task-2/task-6.mongodb.js
@@ -0,0 +1,32 @@
+/**
+ * * 6: Calculate the total balance of individuals
+ * * for each company and display the company name
+ * * along with the total balance. 
+ * * Limit the result to show only
+ * * the top two companies with 
+ * * the highest total balance.
+ */
+use("practice")
+
+db.massive.aggregate([
+    {
+        $project: {
+          balance: 1,
+          new_balance: {
+            $substr: ["$balance", 1, {
+                $subtract: [
+                    {$strLenCP: "$balance"}, 1
+                ]
+            }]
+          },    
+        }
+    },
+    // {
+    //     $group: {
+    //       _id: "$company",
+    //       totalBalance: {
+    //         $sum: "$balance"
+    //       }
+    //     }
+    // }
+])
